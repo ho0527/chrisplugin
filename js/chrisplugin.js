6b57b9d25924096b58329c90bebd2bd81c92749f
chris960527 <98445729+ho0527@users.noreply.github.com>
2024-08-26 22:10:07 +0800

    new: beta2.13.1

--- js/chrisplugin.js
+++ js/chrisplugin.js
@@ -4367,6 +4367,11 @@ async function importhtml(element,url){
 
 	domgetall(element).forEach(function(event){
 		event.innerHTML=text
+		event.querySelectorAll("script").forEach(function(script){
+			let newscript=document.createElement("script")
+			newscript.textContent=script.textContent
+			event.appendChild(newscript)
+		})
 	})
 }
 
