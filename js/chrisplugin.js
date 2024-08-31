f685b83ce2ebb9cb08646d3282f8758285dfc7ae
chris960527 <98445729+ho0527@users.noreply.github.com>
2024-08-31 21:28:43 +0800

    new: beta2.13.5

--- js/chrisplugin.js
+++ js/chrisplugin.js
@@ -4380,6 +4380,64 @@ async function importhtml(element,url,callback=function(){}){
 }
 
 // testing
+function searchtagdiv(element,searchname,data){
+    let sys = {
+        show: false,
+        search: "",
+        searchresults: [],
+        searchData: data,
+        fun: function(text) {
+            if(text === "") {
+                sys.show = false
+                document.getElementById("searchtagdiv").style.display = "none"
+                return
+            }
+            const searchText = text.toLowerCase()
+            sys.searchresults = sys.searchData.filter(item =>
+                item.toLowerCase().includes(searchText)
+            )
+            sys.searchresults.sort((a, b) => {
+                return a.length - b.length
+            })
+            sys.show = sys.searchresults.length > 0
+            renderResults()
+        }
+    }
+
+    function renderResults() {
+        document.getElementById("searchtagdiv").innerHTML = ""
+        if(sys.show) {
+            sys.searchresults.forEach((item) => {
+                let div = document.createElement("div")
+                div.classList = "tagdiv"
+                div.style.display = "flex"
+                div.style.marginBottom = "8px"
+                div.style.width = "100%"
+                div.style.cursor = "pointer"
+                div.innerHTML = `<div style="margin: 0; white-space: nowrap; width: 50%; font-weight: bolder;">${item}</div>`
+                div.onclick = () => {
+                    document.getElementById(searchname).value = item
+                    sys.search = item
+                    document.getElementById("searchtagdiv").style.display = "none"
+                }
+                document.getElementById("searchtagdiv").appendChild(div)
+            })
+            document.getElementById("searchtagdiv").style.display = "block"
+        } else {
+            document.getElementById("searchtagdiv").style.display = "none"
+        }
+    }
+
+    document.getElementById(searchname).addEventListener("input", function(e) {
+        sys.search = e.target.value
+        sys.fun(sys.search)
+    })
+
+	document.getElementById(element).innerHTML=`
+		<div class="tagdivdiv" id="searchtagdiv" style="display: none;"></div>
+	`
+}
+
 function rangeslider(target=null,value=null,step=null,set=null,range=false,scale=true,labels=true,tooltip=true,style="roundstyle",lebel="",onchangecallback=null,stylelist=null){
 	let input
 	let inputdisplay
